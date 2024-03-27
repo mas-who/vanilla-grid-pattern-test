720a9bcac056a3fb1a15d1222c50d1190d1c31d5
Mason Hu <mason.hu@canonical.com>
2024-03-27 17:12:19 +0200

    iteration-6 attempt

---
+++ src/patterns/p-three-col-grid/App.tsx
@@ -0,0 +1,75 @@
+import windows from '../../assets/windows.png';
+import raspberry from '../../assets/raspberry.png';
+// import multipass from './assets/multipass.png';
+// import lxd from './assets/lxd.png';
+import { FC, ReactNode } from 'react';
+
+interface Props {
+  sgHeader: ReactNode;
+  sgContent: ReactNode;
+  sgFooter: ReactNode;
+}
+
+const SubGridCol: FC<Props> = ({ sgHeader, sgContent, sgFooter }) => {
+  return (
+    <div className="p-3-col-grid__col">
+      <header className="p-3-col-grid__col-header">{sgHeader}</header>
+      <p className="p-3-col-grid__col-content">{sgContent}</p>
+      <footer className="p-3-col-grid__col-footer">
+        <hr />
+        {sgFooter}
+      </footer>
+    </div>
+  );
+};
+
+function App() {
+  return (
+    <div className="app">
+      <div className="demo p-3-col-grid">
+        <div className="p-3-col-grid__title">
+          <a href="#">{'LATEST FROM OUR BLOG >'}</a>
+        </div>
+        <div className='p-3-col-grid__content-grid'>
+          <div className="p-3-col-grid__row">
+            <SubGridCol
+              sgHeader={<img src={windows} />}
+              sgContent="Use the Ubuntu terminal and run Linux applications on Windows. Use the Ubuntu terminal
+          and run Linux applications on Windows."
+              sgFooter={<a href="#">Learn about Ubuntu for WSL</a>}
+            />
+            <SubGridCol
+              sgHeader={<img src={raspberry} />}
+              sgContent="Use your Raspberry Pi as a desktop, server or IoT device with Ubuntu."
+              sgFooter={<a href="#">Learn about Ubuntu for Raspberry Pi</a>}
+            />
+            <SubGridCol
+              sgHeader={<img src={windows} />}
+              sgContent="Spin up Ubuntu VMs on Windows, Mac and Linux."
+              sgFooter={<a href="#">Learn about Multipass</a>}
+            />
+          </div>
+          <div className="p-3-col-grid__row">
+            <SubGridCol
+              sgHeader={<img src={windows} />}
+              sgContent="Fast, dense, and secure container and VM management at any scale."
+              sgFooter={<a href="#">Learn about LXD</a>}
+            />
+            <SubGridCol
+              sgHeader={<img src={raspberry} />}
+              sgContent="Spin up Ubuntu VMs on Windows, Mac and Linux."
+              sgFooter={<a href="#">Learn about Multipass</a>}
+            />
+            <SubGridCol
+              sgHeader={<img src={windows} />}
+              sgContent="Use your Raspberry Pi as a desktop, server or IoT device with Ubuntu."
+              sgFooter={<a href="#">Learn about Ubuntu for Raspberry Pi</a>}
+            />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
